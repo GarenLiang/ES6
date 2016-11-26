71b07bbd1c19077cb493acdb5866fd2f02cb8b4c
JackLiang1 <ideabreeze@gmail.com>
2016-11-25 20:36:06 -0600

    Update Filter.js

--- Filter.js
+++ Filter.js
@@ -1,4 +1,4 @@
-//Create a function called 'reject'.  Reject should work in the opposite way of 'filter' - if a function returns 'true', the item should *not* be included in the new array.  Hint: you can reuse filter.
+/*Create a function called 'reject'.  Reject should work in the opposite way of 'filter' - if a function returns 'true', the item should *not* be included in the new array.  Hint: you can reuse filter.
 
 For example:
 
@@ -7,4 +7,4 @@ var lessThanFifteen = reject(numbers, function(number){
   return number > 15;
 }); 
 lessThanFifteen // [ 10 ];
-//
+*/
